a5078517f6af262c49989de593b290bc0eeaeb47
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2023-02-14 18:59:18 +0600

    User login

--- login_app/forms.py
+++ login_app/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     email = forms.EmailField(required=True,label="",widget=forms.TextInput(attrs={'placeholder':'Email'}))
     username = forms.CharField(required=True,label="",widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    password1 = forms.CharField(required=True,label="",widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    password2 = forms.CharField(required=True,label="",widget=forms.PasswordInput(attrs={'placeholder':'Password confirmation'}))
     class Meta:
         model = User
         fields = ('email','username',)
--- login_app/views.py
+++ login_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,HttpResponseRedirect
 from .forms import CreateUserForm
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse,reverse_lazy
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def sign_up(request):
@@ -11,9 +14,29 @@ def sign_up(request):
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user=form.save()
             registered = True
-            pass
+            user_profile = UserProfile(user=user)
+            return HttpResponseredirect(reverse('login_app:login'))
 
     diction = {'title':'Sign Up','form':form}
     return render(request,'login_app/signup.html',context=diction)
+
+def log_in(request):
+    form = AuthenticationForm
+    if request.method=='POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.clean()
+            user = authenticate(username=cleaned_data['username'],password=cleaned_data['password'])
+
+            if user is not None:
+                login(request,user)
+                pass
+    diction = {'title':'Log In','form':form}
+    return render(request,'login_app/login.html',context=diction)
+
+@login_required
+def edit_profile(request):
+    pass
